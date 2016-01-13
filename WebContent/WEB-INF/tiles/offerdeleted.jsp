6587631bdd81f2116dd84afd52168ac564451bb0
sefrota <sergio_letras@hotmail.com>
2016-01-13 14:22:23 +0000

    Pre-Hibernate 2

---
+++ WebContent/WEB-INF/tiles/offerdeleted.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+    pageEncoding="US-ASCII"%>
+Offer deleted: <a href="${pageContext.request.contextPath}/createoffer">Click here to create a new offer.</a>
---
+++ src/com/braincode/soft/dao/OfferRowMapper.java
@@ -0,0 +1,29 @@
+package com.braincode.soft.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+public class OfferRowMapper implements RowMapper<Offer> {
+
+	@Override
+	public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		User user = new User();
+		user.setAuthority(rs.getString("authority"));
+		user.setEmail(rs.getString("email"));
+		user.setEnabled(true);
+		user.setName(rs.getString("name"));
+		user.setUsername(rs.getString("username"));
+		
+		Offer offer = new Offer();
+
+		offer.setId(rs.getInt("id"));
+		offer.setUser(user);
+		offer.setText(rs.getString("text"));
+		return offer;
+	}
+
+
+
+}
